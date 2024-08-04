f09b88c7c0ad87e467a37c659b004470687a3e4d
MIke Shields <codingshields@gmail.com>
2024-08-04 13:07:29 -0600

    Reapply "test"
    This reverts commit e03c1685c913fe486690df5f20a0f4e123c8fece.

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 
 const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const firebaseConfig = {
@@ -19,7 +14,6 @@ const firebaseConfig = {
 	appId: "1:828016954979:web:ee3be9cb357845a1d37256",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
